35e45cdfd3cbf017cd819685c2c28fec39f19b5c
24LP2104 24LP2104 <24LP2104@G302-CC0097.local>
2024-08-06 11:28:51 +0800

    weather

--- tasks/dawtalt/index.js
+++ tasks/dawtalt/index.js
@@ -3,7 +3,7 @@ const weathers = [
     city: "New York",
     temperature: 28,
     weather: "Sunny",
-    icon: "sun",
+    icon: "sunny",
     day: "day",
     time: new Date().toLocaleTimeString("en-US"),
   },
@@ -35,34 +35,73 @@ const weathers = [
     city: "Paris",
     temperature: 25,
     weather: "Clear",
-    icon: "moon",
+    icon: "snowy",
     day: "night",
     time: new Date().toLocaleTimeString("fr-FR"),
   },
 ];
+//icon component
 
-const weather = document.getElementById("main");
+// const iconComponent = (icon) => {
+//   if (icon === "sunny") {
+//     return `<i class="fa-solid fa-sun"></i>`;
+//   } else if (icon === "cloudy") {
+//     return `<i class="fa-solid fa-cloud-sun"></i>`;
+//   } else if (icon === "rainy") {
+//     return `<i class="fa-solid fa-cloud-showers-heavy"></i>`;
+//   } else if (icon === "snowy") {
+//     return `<i class="fa-solid fa-snowflake"></i>`;
+//   }
+// };
 
-weathers.forEach((element, index) => {
-  weather.innerHTML += `
-          <div class="mainWeather">
-            <div class="location">
-              <div class="left">
-                <div>
-                  <h1>${element.city}</h1>
-                  <p class="time">${element.time}</p>
-                </div>
-                <div>
-                  <p>${weather}</p>
-                </div>
-              </div>
-              <div class="right">
-                <div class="gradus">${temperature}<sup>o</sup></div>
-                <div class="icon">
-                  <i class="fa-solid fa-sun"></i>
-                  <i class="fa-solid fa-cloud"></i>
-                </div>
-              </div>
-            </div>
-          </div>`;
-});
+const testobject = {
+  sunny: {
+    icon: `<i class="fa-solid fa-snowflake"></i>`,
+    dayBackground: "./",
+    nightBackground: "./",
+  },
+  cloudy: { icon: "ddd", dayBackground: "./", nightBackground: "./" },
+  rainy: { icon: "ddd", dayBackground: "./", nightBackground: "./" },
+  snowy: { icon: "ddd", dayBackground: "./", nightBackground: "./" },
+};
+// card component
+const cardComponent = () => {
+  weathers.forEach(({ city, temperature, weather, time, day, icon }) => {
+    const result = iconComponent(icon);
+    const boards = document.getElementById("main");
+    // let bgimage;
+    // if (day === "day") {
+
+    //   }
+    //   bgimage = "./img/sky-sunny-clouds-cloudy.webp";
+    // } else if (day === "night") {
+    //   bgimage = "./img/pexels-photo-10317078.webp";
+    // }
+
+    boards.innerHTML += `
+    <div class="mainWeather">
+      <div id="location" style="background-image: url(${
+        day == "day"
+          ? testobject[day].dayBackground
+          : testobject[day].nightBackground
+      });">
+        <div class="left">
+          <div>
+            <h1>${city}</h1>
+            <p class="time">${time}</p>
+          </div>
+          <div>
+            <p>${weather}</p>
+          </div>
+        </div>
+        <div class="right">
+          <div class="gradus">${temperature}<sup>o</sup></div>
+          <div class="icon">
+            ${testobject[day].icon}
+          </div>
+        </div>
+      </div>
+    </div>`;
+  });
+};
+cardComponent();
